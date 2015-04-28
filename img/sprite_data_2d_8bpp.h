80a12270bf1002ff3a88c3916974d285c3c00e7b
Andre Saboia <anboia@gmail.com>
2015-04-28 06:37:09 -0500

    with dude animation

--- img/sprite_data_2d_8bpp.h
+++ img/sprite_data_2d_8bpp.h
@@ -8,7 +8,7 @@
 //	+ 256 tiles not compressed
 //	Total size: 512 + 16384 = 16896
 //
-//	Time-stamp: 2015-04-28, 03:18:39
+//	Time-stamp: 2015-04-28, 05:38:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.12
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,7 +18,7 @@
 #define GRIT_SPRITE_DATA_2D_8BPP_H
 
 #define sprite_data_2d_8bppTilesLen 16384
-extern const unsigned char sprite_data_2d_8bppTiles[16384];
+extern const unsigned int sprite_data_2d_8bppTiles[4096];
 
 #define sprite_data_2d_8bppPalLen 512
 extern const unsigned short sprite_data_2d_8bppPal[256];
